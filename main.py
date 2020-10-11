cc2f698716c0b1f2f966f5a6547ebc21317fc3df
Ishan-Kumar2 <ishankumar216@gmail.com>
2020-10-12 00:35:10 +0530

    added detach in decoder

--- main.py
+++ main.py
@@ -3,47 +3,43 @@ from utils import *
 import config as config
 from torchtext.data import metrics
 from torchtext.data import Iterator,BucketIterator
-
 import random
 
-train_iterator=get_loader(config.squad_path)
+train_iterator = get_loader(config.squad_path)
 
-ENC_EMB_DIM=config.ENC_EMB_DIM
-HID_DIM=config.ENC_HID_DIM
-ENC_DROPOUT=config.ENC_DROPOUT
-input_vocab=len(train_iterator.dataset.word2ind_input)
+ENC_EMB_DIM = config.ENC_EMB_DIM
+HID_DIM = config.ENC_HID_DIM
+ENC_DROPOUT = config.ENC_DROPOUT
+input_vocab = len(train_iterator.dataset.word2ind_input)
 
-pretrained_wvs=torch.ones((input_vocab,ENC_EMB_DIM))
+pretrained_wvs = torch.ones((input_vocab,ENC_EMB_DIM))
 
-enc=Encoder_Model(n_vocab=input_vocab,pretrained_vecs=pretrained_wvs
-                  ,embedding_dim=ENC_EMB_DIM,hidden_dim=HID_DIM,dropout=ENC_DROPOUT)
+enc=Encoder_Model(n_vocab=input_vocab,
+                pretrained_vecs=pretrained_wvs,
+                embedding_dim=ENC_EMB_DIM,
+                hidden_dim=HID_DIM,
+                dropout=ENC_DROPOUT)
 
-output_dim=len(train_iterator.dataset.word2ind_output)
-pretrained_wvs=torch.ones((output_dim,config.DEC_EMBEDDING_DIM))
-HID_DIM=config.DEC_HID_DIM
-id2word_output=train_iterator.dataset.id2word_output
+output_dim = len(train_iterator.dataset.word2ind_output)
+pretrained_wvs = torch.ones((output_dim,config.DEC_EMBEDDING_DIM))
+HID_DIM = config.DEC_HID_DIM
+id2word_output = train_iterator.dataset.id2word_output
 print(id2word_output)
-dec=Decoder(output_dim=output_dim,emb_dim=config.DEC_EMBEDDING_DIM,
+dec=Decoder(output_dim=output_dim,
+            emb_dim=config.DEC_EMBEDDING_DIM,
             hid_dim=HID_DIM,
             dropout=config.DEC_DROPOUT,
             pretrained_vecs=pretrained_wvs)
 
-
-device=config.device
-model=Seq2Seq(enc,dec,device).to(device)
-
+device = config.device
+model = Seq2Seq(enc,dec,device).to(device)
 if config.USING_SAVED:
     pass
 else:
     model.apply(init_weights)
 
-optimizer=optim.Adam(model.parameters())
-
-
-criterion=nn.CrossEntropyLoss(ignore_index=0)
-
-
-
+optimizer = optim.Adam(model.parameters())
+criterion = nn.CrossEntropyLoss(ignore_index=0)
 
 print("Hello")
 for i in enumerate(train_iterator):
@@ -54,26 +50,24 @@ for i in enumerate(train_iterator):
 
 def train(model,iterator,optimizer,criterion,clip):
     model.train()
-    epoch_loss=0
+    epoch_loss = 0
 
-    sent_=[]
-    sent_correct_=[]
+    sent_ = []
+    sent_correct_ = []
     for i,batch in enumerate(iterator):
 
         input=batch[0]
         output=batch[2]
         context=batch[1]
 
-
         print(f"Batch Number {i}")
         optimizer.zero_grad()
         output_pred=model(input,output,context)
 
-        if i%10==0:
-
+        if i%10 == 0:
             #for batch_no in range(output_pred.shape[0]):
-            sent=[]
-            sent_correct=[]
+            sent = []
+            sent_correct = []
 
             for k in range(1,output_pred.shape[1]):
                 sent.append(id2word_output[output_pred[0,k,:].argmax().item()])
@@ -81,15 +75,15 @@ def train(model,iterator,optimizer,criterion,clip):
             sent_.append(sent)
             sent_correct_.append(sent_correct)
             return 0,sent_,sent_correct_
-        output_pred=output_pred[:,1:,:]
-        output=output[:,1:]
-        output_dim=output_pred.shape[-1]
-        output_pred=output_pred.reshape(-1,output_dim)
+        output_pred = output_pred[:,1:,:]
+        output = output[:,1:]
+        output_dim = output_pred.shape[-1]
+        output_pred = output_pred.reshape(-1,output_dim)
 
-        output=output.reshape(output.shape[0]*output.shape[1])
+        output = output.reshape(output.shape[0]*output.shape[1])
 
-        loss=criterion(output_pred,output)
-        epoch_loss+=loss.item()
+        loss = criterion(output_pred,output)
+        epoch_loss += loss.item()
         loss.backward()
         print(loss)
 
@@ -103,7 +97,7 @@ N_EPOCHS=1#config.N_EPOCHS
 CLIP=config.CLIP
 loss_epochs=[]
 for epoch in range(N_EPOCHS):
-    train_loss,sent_,sent_correct_=train(model,train_iterator,optimizer,criterion,CLIP)
+    train_loss,sent_,sent_correct_ = train(model,train_iterator,optimizer,criterion,CLIP)
     print(f"Epoch Number{epoch} Train LOSS {train_loss: .3f} ")
     loss_epochs.append(train_loss)
 for i in range(len(sent_)):
--- model.py
+++ model.py
@@ -37,60 +37,60 @@ class Encoder_Model(nn.Module):
     """
     def __init__(self,n_vocab,
                 pretrained_vecs,
-                arch=1,
-                embedding_dim=100,
-                hidden_dim=64,
-                num_layers=4,
-                bidirectional=True,
-                dropout=0.3):
+                arch = 1,
+                embedding_dim = 100,
+                hidden_dim = 64,
+                num_layers = 4,
+                bidirectional = True,
+                dropout = 0.3):
 
         super(Encoder_Model,self).__init__()
-        self.n_vocab=n_vocab
-        #self.batch_size=batch_size
-        self.embedding_dim=embedding_dim
-        self.hidden_dim=hidden_dim
-        self.num_layers=num_layers
-        self.bidirectional=bidirectional
-        self.arch=arch
-        self.dropout=dropout
-        self.pretrained_vecs=pretrained_vecs
+        self.n_vocab = n_vocab
+        #self.batch_size = batch_size
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.bidirectional = bidirectional
+        self.arch = arch
+        self.dropout = dropout
+        self.pretrained_vecs = pretrained_vecs
 
-        self.embedding=nn.Embedding(self.n_vocab,self.embedding_dim)
+        self.embedding = nn.Embedding(self.n_vocab,self.embedding_dim)
         self.embedding.weight.data.copy_(self.pretrained_vecs)
-        self.embedding.weight.requires_grad=False
+        self.embedding.weight.requires_grad = False
 
         #Encoder
-        if self.arch==1:
-            self.rnn=nn.RNN(self.embedding_dim,self.hidden_dim,
-                           num_layers=self.num_layers,
-                           bidirectional=self.bidirectional,
-                           batch_first=True)
-        if self.arch==2:
-            self.rnn=nn.LSTM(self.embedding_dim,self.hidden_dim,
-                           num_layers=self.num_layers,
-                           bidirectional=self.bidirectional,
-                           batch_first=True)
-        if self.arch==3:
-            self.rnn=nn.GRU(self.embedding_dim,self.hidden_dim,
-                           num_layers=self.num_layers,
-                           bidirectional=self.bidirectional,
-                           batch_first=True)
+        if self.arch == 1:
+            self.rnn = nn.RNN(self.embedding_dim,self.hidden_dim,
+                           num_layers = self.num_layers,
+                           bidirectional = self.bidirectional,
+                           batch_first = True)
+        if self.arch == 2:
+            self.rnn = nn.LSTM(self.embedding_dim,self.hidden_dim,
+                           num_layers = self.num_layers,
+                           bidirectional = self.bidirectional,
+                           batch_first = True)
+        if self.arch == 3:
+            self.rnn = nn.GRU(self.embedding_dim,self.hidden_dim,
+                           num_layers = self.num_layers,
+                           bidirectional = self.bidirectional,
+                           batch_first = True)
 
-        #self.dropout=nn.Dropout(dropout)
+        #self.dropout = nn.Dropout(dropout)
 
     def forward(self,src):
         #src [batch_size,seq_len]
-        embedded=self.embedding(src)
+        embedded = self.embedding(src)
         #embedded [batch_size, seq_len, embedding_dim]
-        if self.arch==1:
-            outputs,hidden=self.rnn(embedded)
+        if self.arch == 1:
+            outputs,hidden = self.rnn(embedded)
         else:
-            outputs,(hidden,cell)=self.rnn(embedded)
+            outputs,(hidden,cell) = self.rnn(embedded)
         #output [batch_size, seq_len, hidden_dim*dir]  its the top hidden layer drop in this case
         #hidden [num_layers*dir, batch_size, hidden_dim]
         #Cell state if present will be same dim as hidden
         if self.bidirectional:
-            hidden=torch.cat((hidden[-1,:,:],hidden[-2,:,:]),dim=1)
+            hidden = torch.cat((hidden[-1,:,:],hidden[-2,:,:]),dim = 1)
 
         #hidden [batch_size, hidden_dim*2] if bidirectional
         return hidden
@@ -125,62 +125,60 @@ class Decoder(nn.Module):
     """
     def __init__(self,
                 output_dim,
-                emb_dim=100,
-                num_layers=1,
-                arch=1,
-                hid_dim=128,
-                dropout=0.3,
-                pretrained_vecs=None):
+                emb_dim = 100,
+                num_layers = 1,
+                arch = 1,
+                hid_dim = 128,
+                dropout = 0.3,
+                pretrained_vecs = None):
 
         super(Decoder,self).__init__()
-        self.hidden_dim=hid_dim
-        self.embedding_dim=emb_dim
-        self.dropout=dropout
-        self.output_dim=output_dim
-        self.pretrained_vecs=pretrained_vecs
-        self.num_layers=num_layers
-        self.arch=arch
-        self.embedding=nn.Embedding(output_dim,emb_dim) #Output dim is number of words in vocab
+        self.hidden_dim = hid_dim
+        self.embedding_dim = emb_dim
+        self.dropout = dropout
+        self.output_dim = output_dim
+        self.pretrained_vecs = pretrained_vecs
+        self.num_layers = num_layers
+        self.arch = arch
+        self.embedding = nn.Embedding(output_dim,emb_dim) #Output dim is number of words in vocab
         if self.pretrained_vecs is not None:
             self.embedding.weight.data.copy_(self.pretrained_vecs)
-            self.embedding.weight.requires_grad=False
+            self.embedding.weight.requires_grad = False
 
-        if self.arch==1:
-            self.rnn=nn.RNN(self.embedding_dim,self.hidden_dim,
-                           num_layers=self.num_layers,
-                           batch_first=True)
-        if self.arch==2:
-            self.rnn=nn.LSTM(self.embedding_dim,self.hidden_dim,
-                           num_layers=self.num_layers,
-                           batch_first=True)
-        if self.arch==3:
-            self.rnn=nn.GRU(self.embedding_dim,self.hidden_dim,
-                           num_layers=self.num_layers,
-                           batch_first=True)
+        if self.arch == 1:
+            self.rnn = nn.RNN(self.embedding_dim,self.hidden_dim,
+                           num_layers = self.num_layers,
+                           batch_first = True)
+        if self.arch == 2:
+            self.rnn = nn.LSTM(self.embedding_dim,self.hidden_dim,
+                           num_layers = self.num_layers,
+                           batch_first = True)
+        if self.arch == 3:
+            self.rnn = nn.GRU(self.embedding_dim,self.hidden_dim,
+                           num_layers = self.num_layers,
+                           batch_first = True)
 
+        self.fc_out = nn.Linear(hid_dim+emb_dim,output_dim)
+        self.dropout = nn.Dropout(dropout)
 
-
-        self.fc_out=nn.Linear(hid_dim+emb_dim,output_dim)
-        self.dropout=nn.Dropout(dropout)
-
-    def forward(self,input,hidden_prev,hidden_2=None):
-        input=input.unsqueeze(1)
+    def forward(self,input,hidden_prev,hidden_2 = None):
+        input = input.unsqueeze(1)
         #input [batch_size,1]
-        embedded=self.embedding(input)
+        embedded = self.embedding(input)
         #embedded [batch_size, 1, embedding_dim]
 
         # hidden_prev [num_layers, batch_size, hidden_dim]
-        if self.arch==1:
-            output,hidden=self.rnn(embedded,hidden_prev)
+        if self.arch == 1:
+            output,hidden = self.rnn(embedded,hidden_prev)
         else:
-            output,(hidden,cell)=self.rnn(embedded,hidden_prev)
+            output,(hidden,cell) = self.rnn(embedded,hidden_prev)
 
         #Output [batch_size, seq_len , hid_dim] The one we threw away in encoder
         #hidden [num_layers, batch_size, hid_dim]
 
-        output=torch.cat((output,embedded),dim=2)
+        output = torch.cat((output,embedded),dim = 2)
         #output [batch_size, seq_len,hidden+embedded]
-        prediction=self.fc_out(output)
+        prediction = self.fc_out(output)
         return prediction,hidden
 
 
@@ -198,47 +196,47 @@ class Seq2Seq(nn.Module):
         Fraction of the time actual output is given to decoder as input rather than prev output
         Default - 0.5
     """
-    def __init__(self,encoder,decoder,device,teacher_forcing_ratio=0.5):
+    def __init__(self,encoder,decoder,device,teacher_forcing_ratio = 0.5):
         super().__init__()
-        self.encoder=encoder
-        self.decoder=decoder
-        self.device=device
-        self.teacher_forcing_ratio=0.01
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+        self.teacher_forcing_ratio = 0.01
 
-    def forward(self,src_batch,trg_batch,context_batch=None):
-        batch_size=src_batch.shape[0]
-        trg_len=trg_batch.shape[1]
-        trg_vocab_size=self.decoder.output_dim
+    def forward(self,src_batch,trg_batch,context_batch = None):
+        batch_size = src_batch.shape[0]
+        trg_len = trg_batch.shape[1]
+        trg_vocab_size = self.decoder.output_dim
 
-        outputs=torch.zeros(batch_size,trg_len,trg_vocab_size).to(self.device)
+        outputs = torch.zeros(batch_size,trg_len,trg_vocab_size).to(self.device)
         #tensor to store probabilities of each word at each time step
 
-        context_vector=self.encoder(src_batch)
+        context_vector = self.encoder(src_batch)
         #context_vector [batch_size, encoder_hidden_size*dir]
 
         if context_batch is not None:
-            context_vector2=self.encoder(context_batch)
-            context_vector=torch.cat((context_vector,context_vector2),dim=1)
+            context_vector2 = self.encoder(context_batch)
+            context_vector = torch.cat((context_vector,context_vector2),dim = 1)
 
-        hidden=context_vector.unsqueeze(0).repeat(self.decoder.num_layers,1,1)
+        hidden = context_vector.unsqueeze(0).repeat(self.decoder.num_layers,1,1)
         #hidden_2 is a copy of context, can feed it to decoder fc at all time steps also
-        hidden_2=context_vector
+        hidden_2 = context_vector
 
         #First input should be <START> token
-        input=torch.tensor(trg_batch[:,0])
+        input = torch.tensor(trg_batch[:,0])
         for t in range(1,trg_len):
-            output,hidden=self.decoder(input,hidden,hidden_2)
+            output,hidden = self.decoder(input,hidden,hidden_2)
             #hidden [1(seq_len), batch_size, dec_hidden]
             #output [1(seq_len), batch_size, vocab_size(dec)]
 
-            output=output.squeeze(0)
-            outputs[:,t:t+1,:]=output
+            output = output.squeeze(0)
+            outputs[:,t:t+1,:] = output
 
-            top1=output.argmax(2)
+            top1 = output.argmax(2)
 
             if(random.random()<self.teacher_forcing_ratio):
-                input=trg_batch[:,t]
+                input = trg_batch[:,t]
             else:
-                input=top1.squeeze(1)
+                input = top1.squeeze(1).detach()  #If this detach is left out the computational graph is retained.
 
         return outputs
